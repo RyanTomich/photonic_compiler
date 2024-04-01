522cf5faacfa871e01225122c202f6d9cd85e574
Ryan Tomich <rjtomich@mit.edu>
2024-04-01 10:31:24 -0400

    Further Testing of alexnet-12.onnx Start of LeNet from relay paper using Decorators (unfinished)

--- analysis.py
+++ analysis.py
@@ -6,13 +6,12 @@ from torchsummary import summary
 
 ### ONNX
 # # load the onnx model
-# onnx_model_path = 'bvlcalexnet-12.onnx'
+# onnx_model_path = 'ONNX-AlexNet/bvlcalexnet-12.onnx'
 # session = onnxruntime.InferenceSession(onnx_model_path)
 
 # # get input layer name and size
 # input_name = session.get_inputs()[0].name
 # input_shape = session.get_inputs()[0].shape
-
 # print(f"{input_name}, {input_shape}")
 
 
@@ -25,14 +24,12 @@ from torchsummary import summary
 
 # print(f"{input_name}: {input_shape}")
 
-# ### Pytorch
-model = torch.load("Pytorch/LeNet.pt")
-
+### Pytorch
+model = torch.load("Pytorch-LeNet/LeNet.pt")
 summary(model, (1, 13, 13))
 
 
-
-### TVM Things
-# model = tvmc.load('Pytorch/LeNet.pt')
+### TVM
+# model = tvmc.load('Pytorch-LeNet/LeNet.pt')
 # model.summary()
 # package = tvmc.compile(model, target = "llvm")
