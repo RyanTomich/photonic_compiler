639096b2faf1f9a687a5450a9083f0bae18df7a5
Ryan Tomich <rjtomich@mit.edu>
2024-04-04 21:53:32 -0400

    Better Json visualization

--- json_parser/parser.py
+++ json_parser/parser.py
@@ -6,28 +6,23 @@ with open('/home/rjtomich/photonic_compiler/Pytorch-LeNet/LeNet_graph.json') as 
 parsed_txt = open("parsed.txt", "w") # creates the write file in write mode append ('a') mode also exists
 
 
-# # Nicer visualization
-# if "nodes" in data:
-#     for num, node in enumerate(data['nodes']):
-#         parsed_txt.write(f"nodes[{num}]:\n")
-#         for key in node:
-#             if key == "attrs":
-#                 parsed_txt.write(f"         {key}: \n")
-#                 for attribute in node[key]:
-#                     parsed_txt.write(f"           {attribute}: {node[key][attribute]}\n")
-#                 continue
-#             parsed_txt.write(f"         {key}: {node[key]}\n")
-
-
-#         # print(f"nodes[{num}]: {node}")
-#         # print("")
-
-# if "attrs" in data:
-#     attrs = data["attrs"]
-#     if "shape" in attrs:
-#         parsed_txt.write(f"Shape attributes: {attrs['shape'][1]}")
-
-
-
+# Nicer visualization
 if "nodes" in data:
-    print(type(data["nodes"]))
+    for num, node in enumerate(data['nodes']):
+        parsed_txt.write(f"nodes[{num}]:\n")
+        for key in node:
+            if key == "attrs":
+                parsed_txt.write(f"         {key}: \n")
+                for attribute in node[key]:
+                    parsed_txt.write(f"           {attribute}: {node[key][attribute]}\n")
+                continue
+            parsed_txt.write(f"         {key}: {node[key]}\n")
+
+
+        # print(f"nodes[{num}]: {node}")
+        # print("")
+
+if "attrs" in data:
+    attrs = data["attrs"]
+    if "shape" in attrs:
+        parsed_txt.write(f"Shape attributes: {attrs['shape'][1]}")
