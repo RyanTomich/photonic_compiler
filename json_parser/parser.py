0969f45f910383fa863d37cf571f22b155115bd6
Ryan Tomich <rjtomich@mit.edu>
2024-04-08 16:23:14 -0400

    Parser seperates matrix multiplication into dot products.

--- json_parser/parser.py
+++ json_parser/parser.py
@@ -40,16 +40,23 @@ def contains(node, val):
 def get_shape_index(node):
     return[input[0] for input in node["inputs"]]
 
+tab = "    " # standard 8 space tab
 
 for order, node in enumerate(raw_json["nodes"]):
     if contains(node, 'null'):
-        parsed_txt.write(f"[null]    {raw_json['nodes'][order]}\n")
+        parsed_txt.write(f"[null]{tab}{raw_json['nodes'][order]}\n")
     elif contains(node, 'dense'):
         parsed_txt.write(f"[relu/MAC]{raw_json['nodes'][order]}\n")
-        parsed_txt.write(f"     input shapes:")
-        for index in get_shape_index(node):
-            parsed_txt.write(f"{str(raw_json['attrs']['shape'][1][index])}")
-        parsed_txt.write("\n")
+        vector_index, matrix_index = get_shape_index(node)
+        vector = raw_json['attrs']['shape'][1][vector_index]
+        matrix = raw_json['attrs']['shape'][1][matrix_index]
+        parsed_txt.write(f"{tab}[mac] {vector} x {matrix}\n")
+        for matrix_row in range(matrix[0]):
+            parsed_txt.write(f"{tab*2}{vector} . {matrix}[{matrix_row}]\n")
+        parsed_txt.write(f"{tab}[relu] {vector}\n")
+
+
+
 
     else:
-        parsed_txt.write(f"[]    {raw_json['nodes'][order]}\n")
+        parsed_txt.write(f"[other]    {raw_json['nodes'][order]}\n")
