32711007ac031cf33483b4474b04a8c9c9772beb
Ryan Tomich <rjtomich@mit.edu>
2024-06-04 15:42:08 -0400

    documentation changes, translate to ONNX, then compile to Relay

--- Pytorch-GPT2/transformer_GPT2.py
+++ Pytorch-GPT2/transformer_GPT2.py
@@ -12,55 +12,45 @@ import os
 gpt2 = GPT2LMHeadModel.from_pretrained('gpt2', torchscript=True)# <class 'transformers.models.gpt2.modeling_gpt2.GPT2LMHeadModel'>
 gpt2_tokenizer = GPT2Tokenizer.from_pretrained('gpt2') # <class 'transformers.models.gpt2.tokenization_gpt2.GPT2Tokenizer'>
 
-# infrence
-# prompt = "my favorite music is"
-# input_ids = gpt2_tokenizer(prompt, return_tensors="pt").input_ids
-# gen_tokens = gpt2.generate(input_ids, do_sample=True, temperature=0.9, max_length=100)
-# gen_text = gpt2_tokenizer.batch_decode(gen_tokens)[0]
-# print(gen_text)
-
-# Load ONNX
+# create input
 prompt = "my favorite music is"
 input_ids =  gpt2_tokenizer(prompt, return_tensors="pt").input_ids
-
 gpt2 = gpt2.eval() # change to eval mode
 
-
+# Load ONNX
+# Check if file already exists
 if not os.path.isfile('./gpt2.onnx'):
-    print("File does not exist")
+    print("File does not exist. Creating ONNX model")
 
     # convert to ONNX
     input_names = ["input_ids"]
     output_names = ["output"]
 
-    torch.onnx.export(
-        gpt2, # model to be exported
-        (input_ids,), # model inputs
-        "gpt2.onnx", # where to save the model
-        input_names=input_names, # input names
-        output_names=output_names, # output names
+    torch.onnx.export( # saves a file
+        gpt2,
+        (input_ids,),
+        "gpt2.onnx",
+        input_names=input_names,
+        output_names=output_names,
         dynamic_axes={"input_ids": {0: "batch"}}, # dynamic axis for variable batch size
         opset_version=11 # ONNX version to export the model
     )
     print('EXPORTED TO ONNX')
 else:
-    print("File exists")
-
-    shape_list = ('input_ids', input_ids.shape) # (name, (1, num_input_ids))
-    shape_dict = {'input_ids': input_ids.shape}  # Adjust based on your model's input shape
+    print("ONNX file already exists. Loading file...")
 
     onnx_model = onnx.load("gpt2.onnx")
 
-    print(type(onnx_model))
+    shape_list = ('input_ids', input_ids.shape) # (name, (1, num_input_ids))
+    shape_dict = {'input_ids': input_ids.shape}  # Adjust based on your model's input shape
     onnx.checker.check_model(onnx_model)
-    print(type(onnx_model.graph))
 
     mod, params = relay.frontend.from_onnx(onnx_model, shape_dict)
 
     target = tvm.target.Target("llvm", host="llvm")
     dev = tvm.cpu(0)
     with tvm.transform.PassContext(opt_level=1):
-        lib = relay.build(mod, target=target, params=params) #module
+        lib = relay.build(mod, target=target, params=params)
 
 
     file_path = "GPT2_model.so"
@@ -72,6 +62,6 @@ else:
         f.write(lib.get_graph_json())
 
     # Save the parameters to a file
-    param_dict = lib.get_params()  # No need to convert to dictionary
+    param_dict = lib.get_params()
     param_bytes_path = "GPT2_params.params"
     tvm.relay.save_param_dict(param_dict)
