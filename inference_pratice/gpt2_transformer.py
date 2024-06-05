5af28488a2964c796d4a579ce4bd61cd107fd57d
Ryan Tomich <rjtomich@mit.edu>
2024-06-05 15:07:40 -0400

    submodule update and comparison update. Validated through first pass attention weights

--- inference_pratice/gpt2_transformer.py
+++ inference_pratice/gpt2_transformer.py
@@ -1,5 +1,6 @@
 import sys
 import os
+import io
 import numpy as np
 
 from huggingface_transformers.src.transformers.models.gpt2.modeling_gpt2 import GPT2LMHeadModel
@@ -11,7 +12,7 @@ gpt2_tokenizer = GPT2Tokenizer.from_pretrained('gpt2') # loading gpt2 tokenizer 
 
 prompt = "my favorite music is"
 input_ids = gpt2_tokenizer(prompt, return_tensors="pt").input_ids
-gen_tokens = gpt2.generate(input_ids, do_sample=True, temperature=0.9, max_length=10)
+gen_tokens = gpt2.generate(input_ids, do_sample=False, temperature=0.9, max_length=6)
 gen_text = gpt2_tokenizer.batch_decode(gen_tokens)[0]
 
 
@@ -19,10 +20,10 @@ gen_text = gpt2_tokenizer.batch_decode(gen_tokens)[0]
 state_dict = gpt2.state_dict()
 parameters = {}
 for name, val in state_dict.items():
-    parameters[name] = val.numpy().astype(np.float32)
+    parameters[name] = np.round(val.numpy().astype(np.float32), 4)
 
 my_model = my_gpt2.MyGPT2(parameters)
-my_result = my_model.generate(prompt, gpt2_tokenizer, max_token_len = 100, num_generate = 10)
+my_result = my_model.generate(prompt, gpt2_tokenizer, max_token_len = 2)
 
-print(f'MY_GPT2:{my_result}')
-print(f'EDITED_TRANSFORMER_GPT2:{gen_text}')
+# print(f'MY_GPT2:{my_result}')
+# print(f'EDITED_TRANSFORMER_GPT2:{gen_text}')
