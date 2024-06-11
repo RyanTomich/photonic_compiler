e3bc3d179ff78e20ae4d0aa6d391bb8bcc2163eb
Ryan Tomich <rjtomich@mit.edu>
2024-06-11 16:33:46 -0400

    CSR format for arbatrary modles

--- model_to_graph/relay_to_adjacency.py
+++ model_to_graph/relay_to_adjacency.py
@@ -6,7 +6,9 @@ import networkx as nx
 
 
 
-read_json_path = '/home/rjtomich/photonic_compiler/model_to_graph/gpt2_graph.json'
+# read_json_path = '/home/rjtomich/photonic_compiler/model_to_graph/gpt2_graph.json'
+read_json_path = '/home/rjtomich/photonic_compiler/model_to_graph/bert-base-uncased_graph.json'
+# read_json_path = '/home/rjtomich/photonic_compiler/Pytorch-LeNet/simple_LeNet_graph.json'
 with open(read_json_path)  as json_file:
     raw_json = json.load(json_file) # returns json file as dict
 
@@ -62,19 +64,34 @@ def creat_dependancy(raw_json):
             dependancys.append((inp[0], node_index)) # (1, 2) where 2 takes 1's output
 
     num_nodes = (len(raw_json['nodes']))
-    adj_matrix = np.empty((num_nodes, num_nodes), int)
+    adj_matrix = np.zeros((num_nodes, num_nodes))
     for dep in dependancys:
-        adj_matrix[dep[0]][dep[1]] = True
+        adj_matrix[dep[0]][dep[1]] = 1
     return adj_matrix
 
-
-
+def matrix_to_CSR(matrix):
+    data = []
+    column_offset = []
+    node_row_ptr = [0,]
+    row_prt_total = 0
+    for row_num, row in enumerate(matrix):
+        for col_num, cell in enumerate(row):
+            if cell:
+                data.append(cell)
+                column_offset.append(col_num)
+                row_prt_total += 1
+        node_row_ptr.append(row_prt_total)
+    return data, column_offset, node_row_ptr
 
 
 node = create_nodes(raw_json)
-dependancy_graph = creat_dependancy(raw_json)
-print(dependancy_graph)
-# print(dependancy_graph)
-print(len(np.nonzero(dependancy_graph)[0]))
+dependancy_matrix = creat_dependancy(raw_json)
+#number of nodes
+print(len(node))
+print(len(raw_json['nodes']))
+print(len(dependancy_matrix))
 
-np.savetxt('dependancy_graph.txt', dependancy_graph)
+print(np.nonzero(dependancy_matrix[54])) # what is a dependancy for node 54
+
+print(len(np.nonzero(dependancy_matrix)[0])) # total number of connections
+data, column_offset, node_row_ptr = matrix_to_CSR(dependancy_matrix)
