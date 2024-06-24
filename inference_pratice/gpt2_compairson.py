059967602f6dfa6d2a22e055c8990abbfdb40670
Ryan Tomich <rjtomich@mit.edu>
2024-06-24 15:06:21 -0400

    record of quantization attempts

--- inference_pratice/gpt2_compairson.py
+++ inference_pratice/gpt2_compairson.py
@@ -9,14 +9,14 @@ from huggingface_transformers.src.transformers.models.gpt2.tokenization_gpt2 imp
 # GPT2-xl - 48 blocks, 24 heads, emb_size 1600
 
 prompt = "I like music that"
+prompt = "Electric Light Orchestra"
 model = 'gpt2'
 max_length = 15
 
 # Out of box Transformer package GPT2
 import np_gpt2 as np_gpt2
-import np_gpt2_int8
-# print("Out of box Transformer")
-# print(np_gpt2.transformer_gpt2_inference(prompt, *np_gpt2.transformer_gpt2_model(model),max_length = max_length))
+print("Out of box Transformer")
+print(np_gpt2.transformer_gpt2_inference(prompt, *np_gpt2.transformer_gpt2_model(model),max_length = max_length))
 
 
 # # Edited Transformer Package GPT2
@@ -25,11 +25,11 @@ t_gpt2_tokenizer = GPT2Tokenizer.from_pretrained(model) # loading gpt2 tokenizer
 
 input_ids = t_gpt2_tokenizer(prompt, return_tensors="pt").input_ids
 
-# t_start_time = time.time()
-# gen_tokens = t_gpt2.generate(input_ids, do_sample=False, max_length=25)
-# t_gen_text = t_gpt2_tokenizer.batch_decode(gen_tokens)[0]
-# t_end_time = time.time()
-# print(t_gen_text)
+t_start_time = time.time()
+gen_tokens = t_gpt2.generate(input_ids, do_sample=False, max_length=25)
+t_gen_text = t_gpt2_tokenizer.batch_decode(gen_tokens)[0]
+t_end_time = time.time()
+print(t_gen_text)
 
 # import model_trace as trace
 
@@ -62,22 +62,22 @@ parameters = np_gpt2.get_parameters(gpt2)
 
 gpt2_model = np_gpt2.NpGPT2(parameters, gpt2_tokenizer, decode_blocks = 12, attn_heads = 12, embedding_size = 768)
 
-# np_start_time = time.time()
-# np_gen_text = gpt2_model.generate(prompt, max_token_len = max_length)
-# np_end_time = time.time()
+np_start_time = time.time()
+np_gen_text = gpt2_model.generate(prompt, max_token_len = max_length)
+np_end_time = time.time()
 
-# print(f'np_GPT2:{np_end_time - np_start_time} \n{np_gen_text}')
+print(f'np_GPT2:{np_end_time - np_start_time} \n{np_gen_text}')
 
 
 
 # int8 GPT2
 
-gpt2_int8 =  np_gpt2_int8.NpGPT2(parameters, gpt2_tokenizer, decode_blocks = 12, attn_heads = 12, embedding_size = 768)
-np_start_time = time.time()
-np_gen_text = gpt2_int8.generate(prompt, max_token_len = max_length)
-np_end_time = time.time()
+# gpt2_int8 =  np_gpt2_int8.NpGPT2(parameters, gpt2_tokenizer, decode_blocks = 12, attn_heads = 12, embedding_size = 768)
+# np_start_time = time.time()
+# np_gen_text = gpt2_int8.generate(prompt, max_token_len = max_length)
+# np_end_time = time.time()
 
-print(f'np_GPT2_int8:{np_end_time - np_start_time} \n{np_gen_text}')
+# print(f'np_GPT2_int8:{np_end_time - np_start_time} \n{np_gen_text}')
 
 # graph_len = len(trace.graph.dependancy_graph)
 # functions_in = trace.file_write.calls
--- inference_pratice/quantized_modles.py
+++ inference_pratice/quantized_modles.py
@@ -3,12 +3,13 @@ import sys
 import onnx
 import onnxruntime as ort
 from onnxruntime import quantization
+from onnxruntime.quantization import quantize_dynamic, QuantType
 import numpy as np
 
 import torch
 from transformers import AutoModelForCausalLM, AutoTokenizer
 
-
+#region ### Creating new ONNX Model
 # filename = '../model_to_graph/Relay_compiler.py'
 # module_name = 'other_script'
 
@@ -19,30 +20,72 @@ from transformers import AutoModelForCausalLM, AutoTokenizer
 # relay_compiler = importlib.util.module_from_spec(spec)
 # spec.loader.exec_module(relay_compiler)
 
-prompt = "my favorite music is"
-model_name = "gpt2"
+# prompt = "my favorite music is"
+# model_name = "gpt2"
 
 # model_onnx, input_ids = relay_compiler.transformer_torch_to_onnx(model_name, prompt, save = False)
+#endregion
 
-model_path = '../model_to_graph/gpt2.onnx'
-onnx_model = onnx.load(model_path)
-onnx.checker.check_model(onnx_model)
+#region ### Working with previous ONNX model
+'''Quantzation with ONNX is skipping the matmul, which defeats the purposes for this project'''
 
-ort_provider = ['CPUExecutionProvider']
-if torch.cuda.is_available():
-    onnx_model.to('cuda')
-    ort_provider = ['CUDAExecutionProvider']
+# def run_model(model_name, model_path, prompt):
+#     onnx_model = onnx.load(model_path)
+#     onnx.checker.check_model(onnx_model)
 
-ort_sess = ort.InferenceSession(model_path, providers=ort_provider)
+#     ort_provider = ['CPUExecutionProvider']
+#     if torch.cuda.is_available():
+#         onnx_model.to('cuda')
+#         ort_provider = ['CUDAExecutionProvider']
 
-tokenizer = AutoTokenizer.from_pretrained(model_name)
-input_ids = tokenizer(prompt, return_tensors="pt").input_ids
-input_ids_numpy = input_ids.cpu().numpy()
+#     ort_sess = ort.InferenceSession(model_path, providers=ort_provider)
 
-ort_inputs = {ort_sess.get_inputs()[0].name: input_ids_numpy}
-ort_outs = ort_sess.run(None, ort_inputs)
+#     tokenizer = AutoTokenizer.from_pretrained(model_name)
+#     input_ids = tokenizer(prompt, return_tensors="pt").input_ids
+#     input_ids_numpy = input_ids.cpu().numpy()
 
-logits = torch.tensor(ort_outs[0][0][-1]) # select the last logit
-predicted_token_id = torch.argmax(logits, dim=-1) # greedly select the largest proability word
-gen_text = tokenizer.batch_decode(np.array([[predicted_token_id]]), skip_special_tokens=True) #
-print(gen_text)
+#     ort_inputs = {ort_sess.get_inputs()[0].name: input_ids_numpy}
+#     ort_outs = ort_sess.run(None, ort_inputs)
+
+#     print(torch.argmax(torch.tensor(ort_outs[0]), dim=-1))
+
+
+
+#     logits = torch.tensor(ort_outs[0][0][-1]) # select the last logit
+#     predicted_token_id = torch.argmax(logits, dim=-1) # greedly select the largest proability word
+#     gen_text = tokenizer.batch_decode(np.array([[predicted_token_id]]), skip_special_tokens=True) #
+#     return gen_text
+
+
+# prompt = "my favorite music is"
+# model_name = "gpt2"
+# model_path = '../model_to_graph/gpt2.onnx'
+# print(run_model(model_name, model_path, prompt))
+
+# # Dynamic Quantization
+# quant_model_path = 'gpt2_prep.onnx'
+# quantize_dynamic(model_path,
+#                 quant_model_path,
+#                 weight_type=QuantType.QInt8)
+# print(f"quantized model saved to:{quant_model_path}")
+
+# print(run_model(model_name, quant_model_path, prompt))
+
+# endregion
+
+#region### my_nlp_architect
+'''well cited but appears to have dependancy issues'''
+# https://github.com/IntelLabs/nlp-architect
+# from transformers import BertTokenizer
+# from my_nlp_architect.nlp_architect.models.transformers.quantized_bert import QuantizedBertPreTrainedModel
+
+
+# model = QuantizedBertPreTrainedModel('quant_bert', from_8bit=True)
+
+# model.save_pretrained('BERTint8')
+
+#endregion
+
+#region ### ZeroQuant
+
+#endregion
