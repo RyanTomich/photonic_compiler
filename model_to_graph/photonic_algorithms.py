ee1cf168781f2e80c0d0160d6cbc724580b653bf
Ryan Tomich <rjtomich@mit.edu>
2024-07-10 11:17:22 -0400

    Individual multiplexed instrutions

--- model_to_graph/photonic_algorithms.py
+++ model_to_graph/photonic_algorithms.py
@@ -1,3 +1,6 @@
+import operator_calcs as oc
+
+# Create Dot Products
 def matmul(m1, m2, preamble):
     """createss dot products from 2 matricies
 
@@ -11,10 +14,10 @@ def matmul(m1, m2, preamble):
     """
     for row in range(m1[-2]):
         for col in range(m2[-2]):
-            yield f'm1{preamble + [row, ':']} * m2{preamble + [col, ':']}  -  {m1[-1]}'
+            yield (preamble + (row, ':'), preamble + (col, ':'), m1[-1]) # (v1, v2, size)
+            # yield f'm1{preamble + [row, ':']} * m2{preamble + [col, ':']}  -  {m1[-1]}'
 
-
-def nd_tensor_product(m1, m2, preamble = []):
+def nd_tensor_product(m1, m2, preamble = ()):
     """Breaks tensor down to the level of matrix multiplication
 
     Args:
@@ -29,6 +32,44 @@ def nd_tensor_product(m1, m2, preamble = []):
        yield from matmul(m1, m2, preamble)
     else:
         for dimention in range(m1[0]):
-            preamble.append(dimention)
+            preamble = preamble + (dimention,)
             yield from nd_tensor_product(m1[1:], m2[1:], preamble=preamble)
-            preamble.pop()
+            preamble = preamble[:-1]
+
+def multiplex_groups(common_operand, unique_operands):
+    full = int(len(unique_operands)/oc.PHU_MULTIPLEX)
+    start = 0
+    end = oc.PHU_MULTIPLEX
+    for i in range(full):
+        yield (common_operand, unique_operands[start:end])
+        start = end
+        end += oc.PHU_MULTIPLEX
+
+    assert end >= len(unique_operands)
+    if len(unique_operands)%oc.PHU_MULTIPLEX:
+        assert end > len(unique_operands)
+        yield (common_operand, unique_operands[start:])
+
+
+
+
+def task_para_node_gen(common_operand, unique_operands):
+    # unique = {}
+    for multiplex in multiplex_groups(common_operand, unique_operands):
+        # dot = f'{multiplex[0]} - {len(multiplex[1])}'
+        # unique.setdefault(dot, 0)
+        # unique[dot] += 1
+
+
+def dynamic_para_node_gen(common_operand, unique_operands):
+    pass
+
+
+node_expansion = {
+    'task_para_matmul_phu': task_para_node_gen,
+    'dynamic_para_matmul_phu': dynamic_para_node_gen,
+    'task_para_dense_phu': task_para_node_gen,
+    'dynamic_para_dense_phu': dynamic_para_node_gen,
+    'task_para_pack_phu': task_para_node_gen,
+    'dynamic_para_pack_phu': dynamic_para_node_gen,
+}
