1c5d955b2bbfcce8a021e011dd614e44f7ddb943
Ryan Tomich <rjtomich@mit.edu>
2024-07-16 15:04:50 -0400

    quantization of Intel/neural-chat-7b-v3-1 and comparison

--- inference_pratice/quantization_comparison.py
+++ inference_pratice/quantization_comparison.py
@@ -1,11 +1,17 @@
+import sys
 import torch
-from transformers import AutoModelForCausalLM, AutoTokenizer, BertTokenizer, BertForMaskedLM, TextStreamer
-
+from transformers import (
+    AutoModelForCausalLM,
+    AutoTokenizer,
+    BertTokenizer,
+    BertForMaskedLM,
+    TextStreamer,
+)
 
 import quantized_models as quantized
 
-def original_gpt(model_name, prompt, max_length):
-    print(model_name)
+
+def original_gpt(model_name, prompt, max_new_tokens, save=False):
     model = AutoModelForCausalLM.from_pretrained(model_name)
     tokenizer = AutoTokenizer.from_pretrained(model_name)
 
@@ -16,24 +22,48 @@ def original_gpt(model_name, prompt, max_length):
 
     attention_mask = input_ids.ne(tokenizer.pad_token_id).long()
 
-    output_ids = model.generate(input_ids, max_length=max_length, attention_mask=attention_mask, pad_token_id=tokenizer.eos_token_id, no_repeat_ngram_size=2)
+    streamer = TextStreamer(tokenizer)
+
+    output_ids = model.generate(
+        input_ids,
+        max_new_tokens=max_new_tokens,
+        attention_mask=attention_mask,
+        pad_token_id=tokenizer.eos_token_id,
+        no_repeat_ngram_size=2,
+        do_sample=False,
+        # streamer = streamer,
+    )
 
     generated_text = tokenizer.decode(output_ids[0], skip_special_tokens=True)
 
-    return generated_text
+    if save:
+        path = f"neural_chat_original.pth"
+        torch.save(model.state_dict(), path)
+
+    return model, generated_text
 
 
 model_name = "Intel/neural-chat-7b-v3-1" # no pytorch?
 # model_name = "gpt2"
 # model_name = "gpt2-medium"
 
-path =  f'{model_name}_quant.pth'
 prompt = "my favorite music is"
 
+max_new_tokens = 50
 
-# print(original_gpt(model_name, prompt, 50))
+
+print(f'############### {model_name} Original Response ###############')
+model, generated = original_gpt(model_name, prompt, max_new_tokens, save=False)
+print(generated)
+# print(quantized.model_param_types(model))
 
 
-print(original_gpt('Intel/neural-chat-7b-v3-1', prompt, 50))
-print(original_gpt('gpt2', prompt, 50))
-print(original_gpt('gpt2-medium', prompt, 50))
+model, generated = quantized.create_quantized_model(model_name, prompt, max_new_tokens, save=False)
+print(f'############### {model_name} Quantized Response ###############')
+print(generated)
+# print(type(model))
+
+# model_quantize_internal: model size  = 27_625.02 MB
+# model_quantize_internal: quant size  =  4316.73 MB
+
+# print(quantized.model_param_types(model))
--- inference_pratice/quantized_models.py
+++ inference_pratice/quantized_models.py
@@ -1,4 +1,4 @@
-#region ### intel extention for transformers
+# region ### intel extention for transformers
 
 from transformers import AutoTokenizer, TextStreamer
 from intel_extension_for_transformers.transformers import AutoModelForCausalLM
@@ -8,31 +8,58 @@ import torchvision.models as modles
 # # # PyTorch Model from Hugging Face
 
 
-def create_model(model_name, path, prompt):
-    '''
+def create_quantized_model(model_name, prompt, max_new_tokens, save):
+    """
     https://github.com/intel/neural-speed/tree/main?tab=readme-ov-file#pytorch-model-from-hugging-face
     does weights for bert, nothing for GPT2
-    '''
-    tokenizer = AutoTokenizer.from_pretrained(model_name, trust_remote_code=True)
-    inputs = tokenizer(prompt, return_tensors="pt").input_ids
-    streamer = TextStreamer(tokenizer)
+    """
     model = AutoModelForCausalLM.from_pretrained(
         model_name,
         load_in_8bit=True,
-    ) # <class 'transformers.models.gpt2.modeling_gpt2.GPT2LMHeadModel'>
+    )
 
-    torch.save(model.state_dict(), path)
-    # outputs = model.generate(inputs, streamer=streamer, max_new_tokens=50)
-    print('saved')
+    tokenizer = AutoTokenizer.from_pretrained(model_name, trust_remote_code=True)
+    tokenizer.padding_side = "right"
+    tokenizer.pad_token = tokenizer.eos_token
 
-def model_param_types(path):
-    checkpoint = torch.load(path)
+    input_ids = tokenizer(prompt, return_tensors="pt", padding=True).input_ids
+    # input_ids = tokenizer(prompt, return_tensors="pt").input_ids
+
+    attention_mask = input_ids.ne(tokenizer.pad_token_id).long()
+
+    streamer = TextStreamer(tokenizer)
+
+    output_ids = model.generate(
+        input_ids,
+        max_new_tokens=max_new_tokens,
+        attention_mask=attention_mask,
+        pad_token_id=tokenizer.eos_token_id,
+        no_repeat_ngram_size=2,
+        do_sample=False,
+        # streamer = streamer,
+    )
+
+    generated_text = tokenizer.decode(output_ids[0], skip_special_tokens=True)
+
+    if save:
+        print(type(model))
+        path = f"neural_chat_quant.pth"
+        # model.save_pretrained(path)
+        # torch.save(model.state_dict(), path)
+
+    return model, generated_text
+
+
+def model_param_types(model):
+    print(type(model))
+    model_weights = model.state_dict()
     types = set()
-    for k, v in checkpoint.items():
+    for k, v in model_weights.items():
         # print(k)
         types.add(v.dtype)
-        if v.dtype == torch.float32:
-            print(f'{type(v)} -- {v.dtype} -- {k}')
+        # if v.dtype == torch.float32:
+            # print(f"{type(v)} -- {v.dtype} -- {k}")
     return types
 
-#endregion
+
+# endregion
