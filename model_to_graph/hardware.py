c34d635d5b980d95b087311ca779fd274ebf16e6
Ryan Tomich <rjtomich@mit.edu>
2024-07-30 16:54:55 -0400

    pass around hardware objects, not srt. updated hardware_algs

--- model_to_graph/hardware.py
+++ model_to_graph/hardware.py
@@ -19,7 +19,6 @@ SRAM_READ = 12 * PICO_JOULE
 SRAM_WRITE = 12 * PICO_JOULE
 LOCAL_READ = 1 * PICO_JOULE
 LOCAL_WRITE = 1 * PICO_JOULE
-GPU_MAC = 0.1 * PICO_JOULE
 PHU_MAC = 0.04 * PICO_JOULE
 
 DAC_POWER = 3.18 * PICO_JOULE
@@ -42,11 +41,13 @@ def ten_elm(tensor_shape):
 
 
 def all_elm(i, o):
-    return {"CPU": ten_elm(o[0])}
+    return ten_elm(o[0])
 
+def standard_energy(i, o):
+    return JOULE_PER_CYCLE
 
 def constnat(c):
-    return lambda i, o: {"CPU": c}
+    return lambda i, o: {cpu: c}
 
 
 def elm_const(matrix, const=1):
@@ -58,34 +59,26 @@ def elm_const(matrix, const=1):
 
 # region Hardware
 class HardwareAlgorithm:
-    def __init__(
-        self,
-        opp,
-        hardware,
-        cycle_function,
-        energy_funcion=lambda i, o: JOULE_PER_CYCLE,
-    ):
+    def __init__(self, opp, cost): #hardware: (time, energy)
         self.opp = opp
-        self.hardware = hardware
-        self.cycle_function = cycle_function
-
-        # default value untill energy for all other algs
-        self.energy_funcion = energy_funcion
+        self.cost = cost
 
     def time_cost(self, i, o):
-        total = 0
-        for hardware, cycles in self.cycle_function(i, o).items():
-            total += cycle_to_tims[hardware](cycles)
-        return total
+        return sum(hardware.clock_period * cost_tup[0] for hardware, cost_tup in self.cost.items())
 
     def energy_cost(self, i, o):
-        return self.energy_funcion(i, o)
+        return sum(cost_tup[1] for hardware, cost_tup in self.cost.items())
 
 
 class Hardware:
+    _universal_hardware_ID = 0
     def __init__(self, clock_speed):
         self.clock_speed = clock_speed
         self.clock_period = 1 / clock_speed
+        Hardware._universal_hardware_ID += 1
+
+    def __hash__(self):
+        return hash(Hardware._universal_hardware_ID)
 
 
 class PHU(Hardware):
@@ -93,32 +86,7 @@ class PHU(Hardware):
         super().__init__(clock_speed)
         self.num_numtiplex = num_multiplex
         self.num_cores = num_cores
-        self.hardware_algs = {
-            "task_para_matmul_phu": HardwareAlgorithm(
-                "matmul",
-                "PHU",
-                self._phu_matmul_task_para_cycles,
-                self._phu_matmul_task_para_energy,
-            ),
-            "task_para_dense_phu": HardwareAlgorithm(
-                "dense",
-                "PHU",
-                self._phu_matmul_task_para_cycles,
-                self._phu_matmul_task_para_energy,
-            ),
-            "task_para_pack_phu": HardwareAlgorithm(
-                "pack",
-                "PHU",
-                self._phu_matmul_task_para_cycles,
-                self._phu_matmul_task_para_energy,
-            ),
-            "dot_prod_phu": HardwareAlgorithm(
-                "dot_prod",
-                "PHU",
-                lambda i, o: {"PHU": i[0][-1]},
-                lambda i, o: i[0][-1] * PHU_MAC,
-            ),
-        }
+        self.mac_energy = 0.04 * PICO_JOULE
 
     def _phu_matmul_task_para_cycles(self, i, o):
         num_dot_products = ten_elm(o[0])
@@ -132,47 +100,14 @@ class PHU(Hardware):
     def _phu_matmul_task_para_energy(self, i, o):
         num_dot_products = ten_elm(o[0])
         length_dot_products = i[0][-1]
-        return num_dot_products * length_dot_products * PHU_MAC
+        return num_dot_products * length_dot_products * self.mac_energy
 
 
 class CPU(Hardware):
     def __init__(self, clock_speed, num_cores):
         super().__init__(clock_speed)
         self.num_cores = num_cores
-        self.hardware_algs = {
-            "add": HardwareAlgorithm("add", "CPU", all_elm),
-            "subtract": HardwareAlgorithm("subtract", "CPU", all_elm),
-            "multiply": HardwareAlgorithm("multiply", "CPU", all_elm),
-            "divide": HardwareAlgorithm("divide", "CPU", all_elm),
-            "sqrt": HardwareAlgorithm("sqrt", "CPU", all_elm),
-            "rsqrt": HardwareAlgorithm("rsqrt", "CPU", all_elm),
-            "relu": HardwareAlgorithm("relu", "CPU", all_elm),
-            "tanh": HardwareAlgorithm(
-                "tanh", "CPU", lambda i, o: {"CPU": ten_elm(o[0]) * 4}
-            ),
-            "power": HardwareAlgorithm("power", "CPU", all_elm),
-            "transpose": HardwareAlgorithm("transpose", "CPU", all_elm),
-            "nop": HardwareAlgorithm("nop", "CPU", constnat(1)),
-            "less": HardwareAlgorithm("less", "CPU", constnat(1)),
-            "take": HardwareAlgorithm("take", "CPU", constnat(1)),
-            "mean": HardwareAlgorithm(
-                "mean", "CPU", lambda i, o: {"CPU": (i[0][-1] + 1) * i[0][-2]}
-            ),
-            "softmax": HardwareAlgorithm(
-                "softmax", "CPU", lambda i, o: {"CPU": ten_elm(o[0]) * 6}
-            ),
-            "matmul": HardwareAlgorithm(
-                "matmul", "CPU", self._cpu_matmul_cycles, self._cpu_matmul_energy
-            ),
-            "dense": HardwareAlgorithm(
-                "dense", "CPU", self._cpu_matmul_cycles, self._cpu_matmul_energy
-            ),
-            "pack": HardwareAlgorithm(
-                "pack", "CPU", self._cpu_matmul_cycles, self._cpu_matmul_energy
-            ),
-            "where": HardwareAlgorithm("where", "CPU", constnat(1)),
-            "erf": HardwareAlgorithm("erf", "CPU", constnat(1)),
-        }
+        self.mac_energy = 0.1 * PICO_JOULE
 
     def _cpu_matmul_cycles(self, i, o):
         num_dot_products = ten_elm(o[0])
@@ -183,12 +118,13 @@ class CPU(Hardware):
     def _cpu_matmul_energy(self, i, o):
         num_dot_products = ten_elm(o[0])
         length_dot_products = i[0][-1]
-        return num_dot_products * length_dot_products * GPU_MAC
+        return num_dot_products * length_dot_products * self.mac_energy
 
 
 class GPU(Hardware):
     def __init__(self, clock_speed):
         super().__init__(clock_speed)
+        self.mac_energy = 0.1 * PICO_JOULE
 
 
 class HBM(Hardware):
@@ -203,5 +139,67 @@ class SRAM(Hardware):
 
 # endregion
 
-CPU(10**8, 1)
-PHU(10**10, 64, 20)
+cpu = CPU(10**8, 1)
+phu = PHU(10**10, 64, 20)
+
+hardware_algs = {
+"add": HardwareAlgorithm("add", {cpu: (all_elm, standard_energy)} ),
+"subtract": HardwareAlgorithm("subtract", {cpu: (all_elm, standard_energy)} ),
+"multiply": HardwareAlgorithm("multiply", {cpu: (all_elm, standard_energy)} ),
+"divide": HardwareAlgorithm("divide", {cpu: (all_elm, standard_energy)} ),
+"sqrt": HardwareAlgorithm("sqrt", {cpu: (all_elm, standard_energy)} ),
+"rsqrt": HardwareAlgorithm("rsqrt", {cpu: (all_elm, standard_energy)} ),
+"relu": HardwareAlgorithm("relu", {cpu: (all_elm, standard_energy)} ),
+"tanh": HardwareAlgorithm("tanh", {cpu: (lambda i, o: ten_elm(o[0]) * 4, standard_energy) } ),
+
+"power": HardwareAlgorithm("power", {cpu: (all_elm, standard_energy) }),
+"transpose": HardwareAlgorithm("transpose", {cpu: (all_elm, standard_energy)} ),
+"nop": HardwareAlgorithm("nop", {cpu: (constnat(1), standard_energy)} ),
+"less": HardwareAlgorithm("less", {cpu: (constnat(1), standard_energy)} ),
+"take": HardwareAlgorithm("take", {cpu: (constnat(1), standard_energy)} ),
+"mean": HardwareAlgorithm(
+    "mean", {cpu: (lambda i, o: (i[0][-1] + 1) * i[0][-2], standard_energy) }),
+"softmax": HardwareAlgorithm(
+    "softmax", {cpu: (lambda i, o: ten_elm(o[0]) * 6, standard_energy) }
+),
+"matmul": HardwareAlgorithm(
+    "matmul", {cpu: (cpu._cpu_matmul_cycles, cpu._cpu_matmul_energy)}
+),
+"dense": HardwareAlgorithm(
+    "dense", {cpu: (cpu._cpu_matmul_cycles, cpu._cpu_matmul_energy)}
+),
+"pack": HardwareAlgorithm(
+    "pack", {cpu: (cpu._cpu_matmul_cycles, cpu._cpu_matmul_energy)}
+),
+"where": HardwareAlgorithm("where", {cpu: (constnat(1), standard_energy) }),
+"erf": HardwareAlgorithm("erf",  {cpu: (constnat(1), standard_energy) }),
+"task_para_matmul_phu": HardwareAlgorithm(
+    "matmul",
+    {phu:
+    (phu._phu_matmul_task_para_cycles,
+    phu._phu_matmul_task_para_energy)
+    }
+),
+"task_para_dense_phu": HardwareAlgorithm(
+    "dense",
+    {phu:
+    (phu._phu_matmul_task_para_cycles,
+    phu._phu_matmul_task_para_energy)
+    }
+),
+"task_para_pack_phu": HardwareAlgorithm(
+    "pack",
+    {phu:
+    (phu._phu_matmul_task_para_cycles,
+    phu._phu_matmul_task_para_energy)
+    }
+),
+"dot_prod_phu": HardwareAlgorithm(
+    "dot_prod",
+    {phu:
+    (lambda i, o: i[0][-1],
+    lambda i, o: i[0][-1] * phu.mac_energy)
+    }
+),
+
+}
