38fdbbca2de3ca409028ff4a911b15aee7cacb77
Ryan Tomich <rjtomich@mit.edu>
2024-08-02 12:31:18 -0400

    relay level time benchmarks

--- validation/tvm_benchmarks.py
+++ validation/tvm_benchmarks.py
@@ -1,11 +1,13 @@
+import os
+import io
+import onnx
+import numpy as np
+
 import tvm
 from tvm import relay
 from tvm.contrib import graph_executor
-import onnx
-import numpy as np
-import os
-import io
-from tvm.runtime import profiler_vm
+
+from tvm.contrib.debugger.debug_executor import GraphModuleDebug
 
 
 
@@ -23,22 +25,18 @@ def generate_token(model_name, prompt):
 
     return int(gen_tokens[0][-1])
 
-def generate_token_TVM(modle_name, prompt, benchmark=False, function_benchmark=False):
+def generate_token_TVM(modle_name, prompt, benchmark=False, function_benchmark=False, profile=False):
     tokenizer = AutoTokenizer.from_pretrained(model_name)
     input_ids = tokenizer(prompt, return_tensors="pt").input_ids
+
 
     graph_json_path = f"../model_to_graph/{model_name}_graph.json"
     lib_so_path = f"../model_to_graph/{model_name}_lib.so"
     param_bytes_path = f"../model_to_graph/{model_name}_params.params"
 
-    loaded_json = open(graph_json_path).read()
-    #https://tvm.apache.org/docs/reference/api/python/runtime.html#tvm.runtime.Module
+    loaded_json = open(graph_json_path).read() # str
     loaded_lib = tvm.runtime.load_module(lib_so_path) # tvm.runtime.module.Module
-    loaded_params = bytearray(open(param_bytes_path, "rb").read())
-
-
-    module = graph_executor.create(loaded_json, loaded_lib, tvm.cpu()) # tvm.contrib.graph_executor.GraphModule
-    module.load_params(loaded_params)
+    loaded_params = bytearray(open(param_bytes_path, "rb").read()) # bytearray
 
     module = graph_executor.create(loaded_json, loaded_lib, tvm.cpu()) # tvm.contrib.graph_executor.GraphModule
     module.load_params(loaded_params)
@@ -50,18 +48,23 @@ def generate_token_TVM(modle_name, prompt, benchmark=False, function_benchmark=F
         print(benchmark_results.results)
         print(benchmark_results)
 
-
-        # Profiling the execution
-        profile_results = module.profile(tvm.cpu())
-        print("Profiling Results:")
-        for name, time_cost in profile_results.items():
-            print(f"{name}: {time_cost} seconds")
+        print (f'threads_used : {tvm.runtime.num_threads()}')
 
 
     if function_benchmark:
-        func_name = 'tvmgen_default_fused_nn_dense_2'
+        lib = tvm.runtime.load_module(lib_so_path) # tvm.runtime.module.Module
 
-        time_eval_func = loaded_lib.time_evaluator(func_name, tvm.cpu())
+        dev = tvm.cpu()
+
+        m = GraphModuleDebug(
+            lib["debug_create"]("default", dev),
+            [dev],
+            loaded_json,
+            dump_root="/tmp/tvmdbg",
+        )
+        m.set_input("input_ids", input_ids)
+        m.run()
+        tvm_out = m.get_output(0).numpy()
 
 
     output = module.get_output(0)
@@ -83,5 +86,5 @@ os.environ["NUMEXPR_NUM_THREADS"] = "1"
 torch.set_num_threads(1)
 
 real_token = generate_token(model_name, prompt)
-tvm_token = generate_token_TVM(model_name, prompt, benchmark=True, function_benchmark=False)
+tvm_token = generate_token_TVM(model_name, prompt, benchmark=False, function_benchmark=True)
 assert real_token == tvm_token
